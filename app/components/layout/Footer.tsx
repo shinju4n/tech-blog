035dabd80a387a2a49f344ede7d586129c867864
Tim <ju4nshin@gmail.com>
2023-10-20 18:43:30 +0900

    레이아웃 설정중

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer className="bg-white h-[2rem]">
+      <span>footer</span>
+    </footer>
+  );
+};
+export default Footer;
