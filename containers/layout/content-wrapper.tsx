a2e478023b78e1b89840d56dc0ca2d9570ab1c5d
shinju4n <ju4nshin@gmail.com>
2024-01-28 22:27:11 +0900

    fix: 레이아웃 반응형 수정

--- containers/layout/content-wrapper.tsx
+++ containers/layout/content-wrapper.tsx
@@ -8,7 +8,7 @@ const ContentWrapper: FC<ContentWrapperProps> = ({ children }) => {
   return (
     <div className="w-full flex md:justify-center p-2 md:py-8">
       <Sidebar />
-      <section className="w-[calc(100%-300px)] p-2 md:px-7">
+      <section className="w-full md:w-[calc(100%-300px)] p-2 md:px-7">
         <div className="relative">{children}</div>
       </section>
     </div>
--- containers/layout/header.tsx
+++ containers/layout/header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {}
 
 const Header: FC<HeaderProps> = ({}) => {
   return (
-    <div className="sticky w-full flex justify-between items-center h-12 border-b border-foreground/20 bg-background top-0 px-8">
+    <div className="sticky w-full flex justify-between items-center h-12 border-b border-foreground/20 bg-background top-0 px-8 z-50">
       <Logo />
       <div>검색</div>
       <ThemeToggle />
