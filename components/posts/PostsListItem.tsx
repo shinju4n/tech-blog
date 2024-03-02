5ffe27954a9e94d265d28f585e18e88a5eebf97f
shinju4n <ju4nshin@gmail.com>
2024-03-02 16:38:31 +0900

    feat : 리스트 호버 시 이미지 호버 애니메이션 추가

--- components/posts/PostsListItem.tsx
+++ components/posts/PostsListItem.tsx
@@ -5,20 +5,22 @@ import Image from "next/image";
 
 const PostListItem: FC<{ post: PostType }> = ({ post }) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-10 w-full h-full bg-background border border-foreground/10 rounded-xl p-4 hover:bg-muted">
+    <div className="flex flex-col sm:flex-row gap-10 w-full h-full bg-background border border-foreground/10 rounded-xl p-4 hover:bg-muted group">
       <div className="flex justify-center items-center w-full sm:max-w-[200px] h-[250px] sm:h-[200px] overflow-hidden bg-foreground/5 ">
         <Image
           src={post.thumbnailUrl}
           alt="thumbnail"
-          width={500}
-          height={500}
-          sizes="5vh"
-          className="transition-all hover:scale-125 ease-in-out duration-300"
+          width={200}
+          height={200}
+          sizes="200p®x"
+          className="transition-all group-hover:scale-125 ease-in-out duration-300"
         />
       </div>
       <div className="w-full flex flex-col justify-between py-2">
         <Typography size="h4">{post.title}</Typography>
-        <Typography size="p">{post.summary}</Typography>
+        <Typography size="p" className="line-clamp-3">
+          {post.summary}
+        </Typography>
         <Typography size="small" className="text-end">
           {post.date}
         </Typography>
