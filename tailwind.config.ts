968c3077834d1921db6a2aaa56b5fd0634fe01f3
shinju4n <ju4nshin@gmail.com>
2024-03-02 16:33:01 +0900

    add : tailwind/line-clamp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,11 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/line-clamp"),
+  ],
 } satisfies Config;
 
 export default config;
