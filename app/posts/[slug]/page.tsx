8860b7e0a2aeeabb9d22ab28428399f28d4ad0dc
shinju4n <ju4nshin@gmail.com>
2024-03-09 15:12:35 +0900

    feat: 메타 동적 태그 구현

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,11 +1,36 @@
-import { type NextPage } from "next";
+import { Metadata, ResolvingMetadata, type NextPage } from "next";
 import { getPost } from "@/service/post-service";
 import MarkdownRender from "@/components/MarkdownRender";
 import PostHead from "@/components/posts/detail/PostHead";
+import Head from "next/head";
+import { Props } from "next/script";
 
 interface PostingDetailProps {
   params: {
     slug: string;
+  };
+  searchParams: Record<string, string | string[] | undefined>;
+}
+
+export async function generateMetadata(
+  { params }: PostingDetailProps,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const post = await getPost(decodeURIComponent(params.slug));
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: post.title,
+    description: post.summary,
+    openGraph: {
+      title: post.title,
+      description: post.summary,
+      locale: "ko_KR",
+      siteName: "주안 블로그",
+      url: "https://ju4n-blog.vercel.app",
+      images: [...previousImages, post.thumbnailUrl],
+    },
   };
 }
 
@@ -13,14 +38,16 @@ const PostingDetail: NextPage<PostingDetailProps> = async ({ params }) => {
   const post = await getPost(decodeURIComponent(params.slug));
 
   return (
-    <div className="w-full lg:max-w-xl xl:max-w-5xl transition-all">
-      <PostHead
-        title={post.title}
-        createdAt={post.date}
-        category={post.category}
-      />
-      <MarkdownRender markdown={post.content} />
-    </div>
+    <>
+      <div className="w-full lg:max-w-xl xl:max-w-5xl transition-all">
+        <PostHead
+          title={post.title}
+          createdAt={post.date}
+          category={post.category}
+        />
+        <MarkdownRender markdown={post.content} />
+      </div>
+    </>
   );
 };
 export default PostingDetail;
