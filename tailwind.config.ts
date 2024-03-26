49a15ea459be4d9ed8f21898c5597fb502d20fb0
Tim <ju4nshin@gmail.com>
2024-03-26 18:44:44 +0900

    delete : tailwind line clamp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,7 +78,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide'), require('@tailwindcss/line-clamp')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 } satisfies Config;
 
 export default config;
