cf04f5c3b0e7d1f65f2751447fd2bce0b1d97577
Tim <ju4nshin@gmail.com>
2024-03-26 12:07:59 +0900

    feat : 포스트 카테고리 컴포넌트 생성

---
+++ components/posts/PostCategoryList.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import Typography from '@/components/ui/typography';
+import PostTagList from '@/components/posts/PostTagList';
+import Badge from '@/components/ui/badge';
+import { cn } from '@/lib/utils';
+
+interface PostCategoryListProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+    category?: string;
+    tag?: string;
+  };
+  tags: string[];
+}
+
+const PostCategoryList: FC<PostCategoryListProps> = ({ searchParams, tags }) => {
+  return (
+    <>
+      <div className="relative">
+        <div className="flex justify-start items-center gap-4 border border-foreground/10 rounded-xl p-4 overflow-x-auto scrollbar-hide">
+          {CATEGORY?.map(category => {
+            return (
+              <Link key={category} href={`/posts?category=${category}`}>
+                <Badge
+                  className={cn(searchParams.category === category && 'ring-2 ring-primary', 'self-stretch')}
+                  label={category}
+                />
+              </Link>
+            );
+          })}
+        </div>
+        <span className="absolute bg-background -top-3 left-2 px-4 text-center">
+          <Typography size="small" className="font-bold">
+            {'> Category'}
+          </Typography>
+        </span>
+      </div>
+      <PostTagList searchParams={searchParams} tags={tags} />
+    </>
+  );
+};
+export default PostCategoryList;
+
+const CATEGORY = ['All', 'FE', 'BE', 'Database'];
