e3281948bf40b66661763189ce449fdfdf67a323
Tim <ju4nshin@gmail.com>
2024-03-26 10:35:15 +0900

    feat : 태그리스트 scroll로 변경

--- components/posts/PostTagList.tsx
+++ components/posts/PostTagList.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import Badge from "../ui/badge";
-import Typography from "../ui/typography";
-import { cn } from "@/lib/utils";
-import { FC } from "react";
+import Link from 'next/link';
+import Badge from '../ui/badge';
+import Typography from '../ui/typography';
+import { cn } from '@/lib/utils';
+import { FC } from 'react';
 
 interface PostTagListProps {
   searchParams: {
@@ -15,24 +15,17 @@ interface PostTagListProps {
 
 const PostTagList: FC<PostTagListProps> = ({ searchParams, tags }) => {
   return (
-    <div className="relative flex flex-wrap justify-evenly sm:justify-start items-center gap-4 border border-foreground/10 rounded-xl p-4">
-      {tags?.map((tag) => {
-        return (
-          <Link
-            key={tag}
-            href={`/posts?category=${searchParams.category}&tag=${tag}`}
-          >
-            <Badge
-              className={cn(
-                searchParams.tag === tag && "ring-2 ring-primary",
-                "self-stretch"
-              )}
-              label={tag}
-            />
-          </Link>
-        );
-      })}
-      <span className="absolute bg-background -top-3 left-3 px-4 text-center">
+    <div className="relative">
+      <div className="flex justify-start items-center gap-4 border border-foreground/10 rounded-xl p-4 overflow-x-auto scrollbar-hide">
+        {tags?.map(tag => {
+          return (
+            <Link key={tag} href={`/posts?category=${searchParams.category}&tag=${tag}`}>
+              <Badge className={cn(searchParams.tag === tag && 'ring-2 ring-primary', 'self-stretch')} label={tag} />
+            </Link>
+          );
+        })}
+      </div>
+      <span className="absolute bg-background -top-3 left-2 px-4 text-center">
         <Typography size="small" className="font-bold">
           # 해시 태그
         </Typography>
