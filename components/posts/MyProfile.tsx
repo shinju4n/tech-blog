57a0f094a8e68ed14bcedccf06879a9d1f821d51
Tim <ju4nshin@gmail.com>
2024-03-29 11:32:05 +0900

    feat: me.ts로 정보 관리

--- components/posts/MyProfile.tsx
+++ components/posts/MyProfile.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Typography from '../ui/typography';
 import Link from 'next/link';
+import { github, email, resume } from '@/lib/me';
 
 const MyProfile: FC = () => {
   return (
@@ -10,21 +11,13 @@ const MyProfile: FC = () => {
         <p className="font-bold text-2xl">신주안</p>
         <Typography size="p">불편함과 문제를 해결하는 것에 보람을 느끼는 프론트엔드 개발자입니다.</Typography>
         <div className="flex gap-4">
-          <Link
-            href={'https://github.com/shinju4n'}
-            target="_blank"
-            className="text-blue-500 whitespace-pre-wrap font-bold"
-          >
+          <Link href={github} target="_blank" className="text-blue-500 whitespace-pre-wrap font-bold">
             Github
           </Link>
-          <Link href={'mailto:ju4nshin@gmail.com'} className="text-blue-500 whitespace-pre-wrap font-bold">
+          <Link href={'mailto:' + email} className="text-blue-500 whitespace-pre-wrap font-bold">
             E-mail
           </Link>
-          <Link
-            href={'https://shinju4n.notion.site/fb02d767e5164273aa2168b899f6022c?pvs=4'}
-            className="text-blue-500 whitespace-pre-wrap font-bold"
-            target="_blank"
-          >
+          <Link href={resume} className="text-blue-500 whitespace-pre-wrap font-bold" target="_blank">
             Resume
           </Link>
         </div>
