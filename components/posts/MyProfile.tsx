1f203333a690a99b805ddd0e59122e42488e0723
Tim <ju4nshin@gmail.com>
2024-03-30 00:04:09 +0900

    feat : my-profile 이미지 min 크기 지정

--- components/posts/MyProfile.tsx
+++ components/posts/MyProfile.tsx
@@ -6,9 +6,9 @@ import { github, email, resume } from '@/lib/me';
 const MyProfile: FC = () => {
   return (
     <section className="flex w-full mb-4 items-center">
-      <div className=" bg-red-500 w-[100px] h-[100px] rounded-full"></div>
+      <div className=" bg-primary min-w-[100px] min-h-[100px] rounded-full"></div>
       <div className="p-5">
-        <p className="font-bold text-2xl">신주안</p>
+        <span className="font-bold text-2xl">신주안</span>
         <Typography size="p">불편함과 문제를 해결하는 것에 보람을 느끼는 프론트엔드 개발자입니다.</Typography>
         <div className="flex gap-4">
           <Link href={github} target="_blank" className="text-blue-500 whitespace-pre-wrap font-bold">
