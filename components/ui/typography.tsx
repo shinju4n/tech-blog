f8740f47ddebe62a47d1b567bf6173ffc223390e
Tim <ju4nshin@gmail.com>
2024-04-01 23:14:53 +0900

    feat : typo h1 사이즈 조정

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -15,7 +15,7 @@ const Typography: FC<TypographyProps> = ({ size = 'small', className, children }
 export default memo(Typography);
 
 const TYPO_STYLES: Record<TypographySize, string> = {
-  h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl',
+  h1: 'scroll-m-20 text-4xl font-extrabold tracking-tight',
   h2: 'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0',
   h3: 'scroll-m-20 text-2xl font-semibold tracking-tight my-6',
   h4: 'scroll-m-20 text-xl font-semibold tracking-tight my-4',
