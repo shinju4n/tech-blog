1c93231b85f5c19e7ca3b7586d39096f5321c4c4
Tim <ju4nshin@gmail.com>
2024-04-01 23:15:15 +0900

    fix: main-wrapeer max-w 에러 수정

--- components/layout/main-wrapper.tsx
+++ components/layout/main-wrapper.tsx
@@ -6,8 +6,8 @@ interface MainWrapperProps {
 
 const MainWrapper: FC<MainWrapperProps> = ({ children }) => {
   return (
-    <main className="flex justify-center w-full px-4 ">
-      <section className="relative w-full xl:max-w-3xl overflow-x-hidden">{children}</section>
+    <main className="flex justify-center w-full px-4">
+      <section className="relative max-w-3xl overflow-x-hidden">{children}</section>
     </main>
   );
 };
