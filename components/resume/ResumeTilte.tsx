6baa032b83a66f50089ad31e616468c2c06424fe
Tim <ju4nshin@gmail.com>
2024-04-08 22:59:38 +0900

    add : Resume Title Comp

---
+++ components/resume/ResumeTilte.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactNode } from 'react';
+import Typography from '@/components/ui/typography';
+
+const ResumeTitle: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <Typography size="h1" className="mb-10">
+      {children}
+    </Typography>
+  );
+};
+
+export default ResumeTitle;
