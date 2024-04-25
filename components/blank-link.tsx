f1b1e7e6b5fd4152b20444706a0ebab206faad21
Tim <ju4nshin@gmail.com>
2024-04-25 18:35:53 +0900

    fix : blank link 스타일 수정

--- components/blank-link.tsx
+++ components/blank-link.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const BlankLink: FC<{ href: string; children: ReactNode }> = ({ href, children }) => {
   return (
     <Link href={href} target="_blank">
-      <span className="text-primary dark:text-cyan-500 transition-colors hover:text-cyan-500 dark:hover:text-cyan-300">
+      <span className="text-primary dark:text-cyan-500 transition-colors hover:text-cyan-500 dark:hover:text-cyan-300 font-bold">
         {children}
       </span>
     </Link>
