62f45e3d63cdaf4367ae2952c518d20c26c332e8
Tim <ju4nshin@gmail.com>
2024-04-25 20:47:51 +0900

    fix : strong 텍스트 색상 변경

--- components/strong.tsx
+++ components/strong.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 
 const Strong: FC<{ children: ReactNode }> = ({ children }) => {
-  return <span className="font-semibold bg-primary text-white px-1">{children}</span>;
+  return <span className="font-bold bg-primary text-primary-foreground px-1 py-[0.2rem]">{children}</span>;
 };
 
 export default Strong;
