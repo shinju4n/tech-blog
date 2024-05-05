5df8384fe6a638353ff0f62c9b6b1c01e6fc1152
Tim <ju4nshin@gmail.com>
2024-05-05 16:02:03 +0900

    fix : 모바일 레이아웃 깨지는 현상 수정

--- components/layout/main-wrapper.tsx
+++ components/layout/main-wrapper.tsx
@@ -7,7 +7,7 @@ interface MainWrapperProps {
 const MainWrapper: FC<MainWrapperProps> = ({ children }) => {
   return (
     <main className="flex justify-center w-full px-4">
-      <section className="relative max-w-3xl h-full">{children}</section>
+      <section className="relative w-full max-w-3xl h-full">{children}</section>
     </main>
   );
 };
