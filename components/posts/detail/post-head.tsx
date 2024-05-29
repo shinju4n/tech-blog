c794e9d5e427edcd48b84347b4fc37b0924b5055
Tim <ju4nshin@gmail.com>
2024-05-29 20:58:57 +0900

    refactor : post title 스타일 변경

--- components/posts/detail/post-head.tsx
+++ components/posts/detail/post-head.tsx
@@ -10,8 +10,8 @@ interface PostHeadProps {
 
 const PostHead: FC<PostHeadProps> = ({ title, createdAt, tags }) => {
   return (
-    <div className="mb-2 border-b border-foreground/30 py-2">
-      <Typography size="h1" className="mb-4">
+    <div className="mb-5 border-b border-foreground/30 py-2">
+      <Typography size="h1" className="my-6">
         {title}
       </Typography>
       <div className="flex flex-col sm:flex-row justify-between gap-2">
@@ -20,7 +20,7 @@ const PostHead: FC<PostHeadProps> = ({ title, createdAt, tags }) => {
           {tags?.map((tag, i) => {
             return (
               <Link key={i} href={`/posts?tag=${tag}`}>
-                <Badge label={tag} />
+                <Badge label={'# ' + tag} />
               </Link>
             );
           })}
