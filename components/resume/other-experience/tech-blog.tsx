aa72459fee6c906cbb555ba4e6fb38e050dbeb74
Tim <ju4nshin@gmail.com>
2024-06-07 15:53:29 +0900

    feat : [이력서] 기술 블로그 내용 보강

--- components/resume/other-experience/tech-blog.tsx
+++ components/resume/other-experience/tech-blog.tsx
@@ -20,10 +20,16 @@ const TechBlog: FC = () => {
             Work.
           </Typography>
           <ul className="pl-4 list-disc flex flex-col gap-1">
-            <li>Next.js의 SSR을 활용하여 SEO를 향상시켰습니다.</li>
+            <li>검색 엔진에서 높은 순위에 노출 되도록 Next.js의 SSR을 활용하여 SEO를 향상시켰습니다.</li>
             <li>포스팅을 MarkDown 파일로 작성하면 시멘틱 태그에 맞게 파싱할 수 있도록 개발하였습니다.</li>
-            <li>반응형 레이아웃과 다크모드를 구현하였습니다.</li>
-            <li>shadcn/ui 라이브러리를 사용하여 디자인 및 컴포넌트를 개발하였습니다.</li>
+            <li>
+              사용자 경험을 향상시키기 위해 다양한 디바이스와 사용 환경에 대응할 수 있도록 반응형 레이아웃과 다크모드를
+              구현하였습니다.
+            </li>
+            <li>
+              기술 블로그를 제작하면서 개발 생산성, UI의 일관성, 반응형 디자인, 커스터마이징 용이성 등을 고려하여
+              shadcn/ui 라이브러리를 선택하여 제작하였습니다.
+            </li>
           </ul>
         </section>
         <section>
