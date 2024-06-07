5714ebc92e3fc9bd2537bd7a3418c72a3a955a71
Tim <ju4nshin@gmail.com>
2024-06-07 18:11:41 +0900

    feat: strong 컴포넌트 padding 값 수정

--- components/strong.tsx
+++ components/strong.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 
 const Strong: FC<{ children: ReactNode }> = ({ children }) => {
-  return <span className="font-bold bg-primary text-primary-foreground px-1 py-[0.2rem]">{children}</span>;
+  return <span className="font-bold bg-primary text-primary-foreground px-0.5">{children}</span>;
 };
 
 export default Strong;
