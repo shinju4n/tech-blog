6d7ed3303b43779b0382bad25bffa35a263c77ab
shinju4n <ju4nshin@gmail.com>
2024-07-03 14:29:25 +0900

    feat : blank link 이모티콘 추가

--- components/blank-link.tsx
+++ components/blank-link.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 const BlankLink: FC<{ href: string; children: ReactNode }> = ({ href, children }) => {
   return (
     <Link href={href} target="_blank" className="inline">
-      <span className="text-primary dark:text-cyan-500 transition-colors hover:text-cyan-500 dark:hover:text-cyan-300 font-bold underline">
-        {children}
+      <span className="text-primary dark:text-cyan-500 transition-colors hover:text-cyan-500 dark:hover:text-cyan-300 font-bold">
+        🔗 {children}
       </span>
     </Link>
   );
