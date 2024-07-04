07ae33ec9c545883b73e1a591c6fd662a6775bb0
shinju4n <ju4nshin@gmail.com>
2024-07-04 11:30:43 +0900

    feat : pinned Post 레이아웃 수정

--- components/posts/pinned-posts.tsx
+++ components/posts/pinned-posts.tsx
@@ -3,25 +3,29 @@ import { FC } from 'react';
 import Image from 'next/image';
 import Typography from '../ui/typography';
 import Link from 'next/link';
-import { cn } from '@/lib/utils';
 
 const PinnedPosts: FC = async () => {
   const pinned = await getPinnedPosts();
   return (
     <>
       <Typography size="h4">📌 Pinned Posts</Typography>
-      <div className="flex overflow-scroll gap-2 w-full scrollbar-hide">
+      <div className="flex w-full mb-4 overflow-scroll gap-2 scrollbar-hide">
         {pinned.map((post, i) => {
           return (
             <Link href={`/posts/${post.id}`} key={i}>
-              <div
-                className={cn(
-                  'flex flex-1 flex-col w-[18rem] h-[15rem] gap-5 justify-center items-center shadow-md',
-                  'bg-background border border-foreground/10 rounded-xl  group overflow-hidden hover:bg-foreground/5'
-                )}
-              >
-                <Thumbnail src={post.thumbnailUrl} />
-                <Content title={post.title} />
+              <div className="flex flex-col justify-center h-full border rounded-lg shadow-md overflow-hidden">
+                <div className="relative w-64 h-32">
+                  <Image
+                    src={post.thumbnailUrl}
+                    alt={post.id + 'thumbnail'}
+                    width={200}
+                    height={100}
+                    loading="lazy"
+                    className="absolute inset-0 w-full h-full object-cover"
+                    sizes="(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 33vw"
+                  />
+                </div>
+                <div className="pl-2 py-2 font-bold line-clamp-2">{post.title}</div>
               </div>
             </Link>
           );
@@ -31,33 +35,3 @@ const PinnedPosts: FC = async () => {
   );
 };
 export default PinnedPosts;
-
-const Thumbnail: FC<{ src: string }> = ({ src }) => {
-  return (
-    <div
-      className={cn('relative flex flex-[2] justify-center items-center w-full h-full overflow-hidden bg-foreground/5')}
-    >
-      <div className="flex justify-center items-center relative w-2/3 min-h-[15rem]">
-        <Image
-          src={src}
-          alt="thumbnail"
-          width={100}
-          height={100}
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          loading="lazy"
-          className="transition-all group-hover:scale-125 ease-in-out duration-300 w-auto h-auto scale-150"
-        />
-      </div>
-    </div>
-  );
-};
-
-const Content: FC<{ title: string }> = ({ title }) => {
-  return (
-    <div className="w-full flex flex-1 flex-col justify-between pb-4 px-4">
-      <Typography size="h4" className="line-clamp-2 font-bold text-xl">
-        {title}
-      </Typography>
-    </div>
-  );
-};
